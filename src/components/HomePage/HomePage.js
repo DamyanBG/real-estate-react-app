f2805c1eaa763914ec527333fa7cad5d7ac76568
DamyanBG <d.dimitrov.1988@abv.bg>
2022-12-22 23:07:14 +0200

    home page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react"
 import "./HomePage.scss"
+import { hostUrl } from "../../common/urls"
 
 export default function HomePage() {
+    const [homes, setHomes] = useState([])
+
+    useEffect(() => {
+        fetchAllHomes()
+    }, [])
+
+    const fetchAllHomes = () => {
+        fetch(`${hostUrl}/homes`)
+            .then(resp => resp.json())
+            .then(setHomes)
+    }
+
     return (
-        <section>
-            bla bla
-        </section>
+        <div className="home-articles-container">
+            {
+                homes.map((h) => (
+                    <article className="home-article">
+                        <h4>{h.name}</h4>
+                        <p>Address: {h.place}</p>
+                        <p>Price: {h.price}</p>
+                    </article>
+                ))
+            }
+        </div>
     )
 }
