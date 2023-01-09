c663d9ce6706077cfccd6aeba8ee9452dfad7297
alivadjid <mshagidullin@rzdit.ru>
2023-01-10 00:07:59 +0300

    feat/admin-page

---
+++ src/components/User/Admin.js
@@ -0,0 +1,9 @@
+export default function Admin() {
+    return (
+        <>
+            <div>Manage users</div>
+            <div>Manage homes</div>
+            <div>Manage lands</div>
+        </>
+    );
+}
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,15 +1,25 @@
 import { createContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { hostUrl } from '../common/urls';
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [userInfo, setUserInfo] = useState({});
+
+    const getUserInfo = async (userId) => {
+        await fetch(`${hostUrl}/user/${userId}`)
+            .then((resp) => resp.json())
+            .then(setUserInfo);
+    };
 
     useEffect(() => {
         const localStorageUser = localStorage.getItem('userId');
+
         if (localStorageUser) {
             setUser(localStorageUser);
+            getUserInfo(localStorageUser);
         }
     }, []);
 
@@ -18,6 +28,8 @@ export const UserProvider = ({ children }) => {
             value={{
                 user,
                 setUser,
+                userInfo,
+                setUserInfo,
             }}
         >
             {children}
