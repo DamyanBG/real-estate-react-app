f5210095d0172a5ae3335662bb8c052aebcb536d
Oderinde Emmanuel Oluwatobiloba <oderinde.oluwatobiloba@gmail.com>
2023-01-14 21:10:10 +0100

    added validations to the home form

--- src/common/InputFormRow.js
+++ src/common/InputFormRow.js
@@ -1,9 +1,15 @@
-export default function InputFormRow({ labelName, name, value, handleOnChange }) {
+export default function InputFormRow({ labelName, name, value, handleOnChange, handleOnBlur }) {
     return (
         <article className="form-row">
             <div className="input-wrapper">
                 <label>{labelName}</label>
-                <input type="text" name={name} value={value || ''} onChange={handleOnChange} />
+                <input
+                    type="text"
+                    name={name}
+                    value={value || ''}
+                    onChange={handleOnChange}
+                    onBlur={handleOnBlur}
+                />
             </div>
         </article>
     );
