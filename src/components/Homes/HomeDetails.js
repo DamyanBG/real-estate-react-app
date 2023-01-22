6920ba90b65164230f6d40074f289cb0456a4eb4
DamyanBG <d.dimitrov.1988@abv.bg>
2023-01-22 17:07:16 +0200

    map to the details

--- src/components/Homes/HomeDetails.js
+++ src/components/Homes/HomeDetails.js
@@ -4,6 +4,7 @@ import { useContext, useEffect, useState } from 'react';
 import { hostUrl } from '../../common/urls';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../../context/UserContext';
+import { MapContainer, Marker, TileLayer, Tooltip } from 'react-leaflet';
 
 export default function HomeDetails() {
     const params = new URLSearchParams(window.location.search);
@@ -23,51 +24,70 @@ export default function HomeDetails() {
     useEffect(fetchHomeDetails, [homeId]);
 
     return (
-        <section className="home-details-container">
-            <div className="home-details-image-container">
-                <img
-                    src={homeDetails.photo_url}
-                    alt="Home"
-                    onError={(e) => {
-                        e.target.onError = null;
-                        e.target.src = exampleHomePhoto;
-                    }}
-                />
-            </div>
-            <div className="home-details-text">
-                <h2>{homeDetails.title}</h2>
-                <p>Location: {homeDetails.city}</p>
-                <p>Neighborhood: {homeDetails.neighborhood}</p>
-                <p>Address: {homeDetails.address}</p>
-                <p>Price: {homeDetails.price}</p>
-                <p>Year: {homeDetails.year}</p>
-                <p>Information: {homeDetails.description}</p>
-                <p>
-                    Owner: {homeDetails.owner_names}{' '}
-                    {user._id && user._id !== homeDetails.owner_id && (
-                        <Link
-                            to={`/chat?interlocutorId=${homeDetails.owner_id}&names=${homeDetails.owner_names}`}
-                        >
-                            <button>Start chat</button>
-                        </Link>
+        <section className="home-details">
+            <section className="home-details-container">
+                <div className="home-details-image-container">
+                    <img
+                        src={homeDetails.photo_url}
+                        alt="Home"
+                        onError={(e) => {
+                            e.target.onError = null;
+                            e.target.src = exampleHomePhoto;
+                        }}
+                    />
+                </div>
+                <div className="home-details-text">
+                    <h2>{homeDetails.title}</h2>
+                    <p>Location: {homeDetails.city}</p>
+                    <p>Neighborhood: {homeDetails.neighborhood}</p>
+                    <p>Address: {homeDetails.address}</p>
+                    <p>Price: {homeDetails.price}</p>
+                    <p>Year: {homeDetails.year}</p>
+                    <p>Information: {homeDetails.description}</p>
+                    <p>
+                        Owner: {homeDetails.owner_names}{' '}
+                        {user._id && user._id !== homeDetails.owner_id && (
+                            <Link
+                                to={`/chat?interlocutorId=${homeDetails.owner_id}&names=${homeDetails.owner_names}`}
+                            >
+                                <button>Start chat</button>
+                            </Link>
+                        )}
+                        {user._id && user._id !== homeDetails.owner_id && (
+                            <Link to={`/create-meeting?createWithId=${homeDetails.owner_id}`}>
+                                <button>Request meeting</button>
+                            </Link>
+                        )}
+                    </p>
+                    {user._id === homeDetails.owner_id && (
+                        <>
+                            <Link to={`/edit-home?homeId=${homeId}`}>
+                                <button>Edit</button>
+                            </Link>
+                            <Link to={`/create-visitation?homeId=${homeId}`}>
+                                <button>Create Visitation</button>
+                            </Link>
+                        </>
                     )}
-                    {user._id && user._id !== homeDetails.owner_id && (
-                        <Link to={`/create-meeting?createWithId=${homeDetails.owner_id}`}>
-                            <button>Request meeting</button>
-                        </Link>
-                    )}
-                </p>
-                {user._id === homeDetails.owner_id && (
-                    <>
-                        <Link to={`/edit-home?homeId=${homeId}`}>
-                            <button>Edit</button>
-                        </Link>
-                        <Link to={`/create-visitation?homeId=${homeId}`}>
-                            <button>Create Visitation</button>
-                        </Link>
-                    </>
-                )}
-            </div>
+                </div>
+            </section>
+            {homeDetails.latitude && homeDetails.longitude && (
+                <article className='map-container'>
+                    <MapContainer
+                        center={[homeDetails.latitude, homeDetails.longitude]}
+                        zoom={13}
+                        scrollWheelZoom={false}
+                        style={{ width: '100%', height: '50vh' }}
+                    >
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[homeDetails.latitude, homeDetails.longitude]}>
+                            <Tooltip direction="bottom">Some text</Tooltip>
+                        </Marker>
+                    </MapContainer>
+                </article>
+            )}
         </section>
     );
 }
--- src/components/Land/LandDetails.js
+++ src/components/Land/LandDetails.js
@@ -4,6 +4,7 @@ import { hostUrl } from '../../common/urls';
 import { UserContext } from '../../context/UserContext';
 import { Link } from 'react-router-dom';
 import './LandDetails.scss';
+import { MapContainer, Marker, TileLayer, Tooltip } from 'react-leaflet';
 
 export default function LandDetails() {
     const params = new URLSearchParams(window.location.search);
@@ -22,49 +23,68 @@ export default function LandDetails() {
     useEffect(fetchLandDetails, [landId]);
 
     return (
-        <section className="home-details-container">
-            <div className="home-details-image-container">
-                <img src={exampleLandPhoto} alt="Land" />
-            </div>
-            <div className="home-details-text">
-                <h2>{landDetails.name}</h2>
-                <p>Location: {landDetails.place}</p>
-                <p>Price: {landDetails.price}</p>
-                <p>
-                    Owner: {landDetails.owner_names}{' '}
-                    {user._id && user._id !== landDetails.owner && (
-                        <Link
-                            to={`/chat?interlocutorId=${landDetails.owner}&names=${landDetails.owner_names}`}
-                        >
-                            <button>Start chat</button>
-                        </Link>
-                    )}{' '}
-                    {user._id && user._id !== landDetails.owner && (
-                        <Link to={`/create-meeting?createWithId=${landDetails.owner}`}>
-                            <button>Request meeting</button>
-                        </Link>
+        <section className='home-details'>
+            <section className="home-details-container">
+                <div className="home-details-image-container">
+                    <img src={exampleLandPhoto} alt="Land" />
+                </div>
+                <div className="home-details-text">
+                    <h2>{landDetails.name}</h2>
+                    <p>Location: {landDetails.place}</p>
+                    <p>Price: {landDetails.price}</p>
+                    <p>
+                        Owner: {landDetails.owner_names}{' '}
+                        {user._id && user._id !== landDetails.owner && (
+                            <Link
+                                to={`/chat?interlocutorId=${landDetails.owner}&names=${landDetails.owner_names}`}
+                            >
+                                <button>Start chat</button>
+                            </Link>
+                        )}{' '}
+                        {user._id && user._id !== landDetails.owner && (
+                            <Link to={`/create-meeting?createWithId=${landDetails.owner}`}>
+                                <button>Request meeting</button>
+                            </Link>
+                        )}
+                    </p>
+                    <p>Information: {landDetails.description}</p>
+                    {user._id === landDetails.owner ? (
+                        <>
+                            <Link to={`/edit-land?landId=${landId}`} className="land-details_edit">
+                                Edit
+                            </Link>
+                        </>
+                    ) : (
+                        ''
                     )}
-                </p>
-                <p>Information: {landDetails.description}</p>
-                {user._id === landDetails.owner ? (
-                    <>
-                        <Link to={`/edit-land?landId=${landId}`} className="land-details_edit">
-                            Edit
-                        </Link>
-                    </>
-                ) : (
-                    ''
-                )}
-                {user._id === landDetails.owner ? (
-                    <>
-                        <Link to={`/create-visitation?landId=${landId}`}>
-                            <button>Create visitation</button>
-                        </Link>
-                    </>
-                ) : (
-                    ''
-                )}
-            </div>
+                    {user._id === landDetails.owner ? (
+                        <>
+                            <Link to={`/create-visitation?landId=${landId}`}>
+                                <button>Create visitation</button>
+                            </Link>
+                        </>
+                    ) : (
+                        ''
+                    )}
+                </div>
+            </section>
+            {landDetails.latitude && landDetails.longitude && (
+                <article className='map-container'>
+                    <MapContainer
+                        center={[landDetails.latitude, landDetails.longitude]}
+                        zoom={13}
+                        scrollWheelZoom={false}
+                        style={{ width: '100%', height: '50vh' }}
+                    >
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[landDetails.latitude, landDetails.longitude]}>
+                            <Tooltip direction="bottom">Some text</Tooltip>
+                        </Marker>
+                    </MapContainer>
+                </article>
+            )}
         </section>
     );
 }
