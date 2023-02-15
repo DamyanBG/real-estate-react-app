e99de4b461c80f8caa10edb285688915575dbebc
Francis <numofran6@gmail.com>
2023-02-15 12:38:54 +0000

    Pagination set for Lands and Homes Page

--- src/components/Homes/AllHomes.js
+++ src/components/Homes/AllHomes.js
@@ -4,9 +4,20 @@ import exampleHomePhoto from '../../images/home-main-photo-example.jpg';
 import './Homes.scss';
 import './AllHomes.scss';
 import { Link } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
 
 export default function AllHomes() {
     const [homes, setHomes] = useState([]);
+
+    // Pagination Configuration
+    const [pageNumber, setPageNumber] = useState(0);
+    const homesPerPage = 10;
+    const pagesVisited = pageNumber * homesPerPage;
+    const pageCount = Math.ceil(homes.length / homesPerPage);
+
+    const pageChangeHandler = ({ selected }) => {
+        setPageNumber(selected);
+    };
 
     useEffect(() => {
         fetchAllHomes();
@@ -22,8 +33,8 @@ export default function AllHomes() {
     };
 
     return (
-        <div className='main-container'>
-            {homes.map((h) => (
+        <div className="main-container">
+            {homes.slice(pagesVisited, pagesVisited + homesPerPage).map((h) => (
                 <article className="home-container" key={`home-${h._id}`}>
                     <div className="image-container">
                         <img
@@ -40,14 +51,26 @@ export default function AllHomes() {
                         <Link to={`/home-details?homeId=${h._id}`} className="no-underline">
                             <h3>{h.title}</h3>
                         </Link>
-                        <p className='main-text-par'>Location: {h.city}</p>
+                        <p className="main-text-par">Location: {h.city}</p>
                         <p>Price: {h.price}</p>
                     </div>
                     <div className="description-text">
-                        <p className='description-text-par'>{h.description}</p>
+                        <p className="description-text-par">{h.description}</p>
                     </div>
                 </article>
             ))}
+
+            <ReactPaginate
+                previousLabel="Previous"
+                nextLabel="Next"
+                pageCount={pageCount}
+                onPageChange={pageChangeHandler}
+                containerClassName="paginationBtns"
+                previousLinkClassName="previousBtn"
+                nextLinkClassName="nextBtn"
+                disabledClassName="paginationDisabled"
+                activeClassName="paginationActive"
+            />
         </div>
     );
 }
