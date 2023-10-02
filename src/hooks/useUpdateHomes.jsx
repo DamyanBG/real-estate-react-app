6a1155462120fa49cd40d941243a65a878bbb391
sirsamuelo <samec070@gmail.com>
2023-10-02 18:37:32 +0200

    switched commonjs to es6 import

--- src/hooks/useUpdateHomes.jsx
+++ src/hooks/useUpdateHomes.jsx
@@ -1,22 +1,22 @@
-const { fetchAllHomes } = require("@/common/homesApi")
-const { HomesDispatchContext } = require("@/context/HomesContext")
-const { useContext } = require("react")
+import { fetchAllHomes } from "@/common/homesApi";
+import { HomesDispatchContext } from "@/context/HomesContext";
+import { useContext } from "react";
 
 const useUpdateHomes = () => {
-    const dispatchHomesContext = useContext(HomesDispatchContext)
+  const dispatchHomesContext = useContext(HomesDispatchContext);
 
-    const updateHomes = () => {
-        console.log("updateding homes")
-        fetchAllHomes()
-            .then((fetchedHomes) => {
-                dispatchHomesContext({
-                    type: "set",
-                    setHomes: fetchedHomes
-                })
-            })
-    }
+  const updateHomes = () => {
+    console.log("updating homes");
+    fetchAllHomes()
+      .then((fetchedHomes) => {
+        dispatchHomesContext({
+          type: "set",
+          setHomes: fetchedHomes
+        });
+      });
+  };
 
-    return updateHomes
-}
+  return updateHomes;
+};
 
-export default useUpdateHomes
+export default useUpdateHomes;
