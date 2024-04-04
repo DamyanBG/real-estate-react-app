c5d812d5bfe4bb37397fe2eabc1ce1f0b9d206b7
DamyanBG <d.dimitrov.1988@abv.bg>
2024-04-04 18:05:52 +0300

    adding old files

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import LandingProperties from '../components/LangingPage/LangingProperties';
+import MainSection from '../components/LangingPage/MainSection';
+
+const LandingPage = () => {
+    return (
+        <>
+            <MainSection />
+            <LandingProperties />
+        </>
+    );
+};
+
+export default LandingPage;
