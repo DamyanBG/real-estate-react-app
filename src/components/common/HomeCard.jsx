42cb03b5bb6c3fb5a3e706fa1d70c7f26b1d1597
Soliheen Farooq Khan <soliheen.farooqkhan@u-next.com>
2024-04-22 10:55:52 +0530

    addedImageAsImport

--- src/components/common/HomeCard.jsx
+++ src/components/common/HomeCard.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
+import defaultImage from '../../../public/images/house-image-mirror.png';
 
 const HomeCard = ({ homeId, photoUrl, city, neighborhood, title, description, price }) => {
     return (
         <article className="property-card">
             <article className="property-image-container">
-                <img src={photoUrl || `../../../public/images/house-image-mirror.png`} alt="Property Image" onError={(e) => {
+                <img src={photoUrl} alt="Property Image" onError={(e) => {
                     e.target.onerror = null; // prevents looping
-                    e.target.src = '../../../public/images/house-image-mirror.png';
+                    e.target.src = defaultImage;
                 }} />
             </article>
 
@@ -15,7 +16,7 @@ const HomeCard = ({ homeId, photoUrl, city, neighborhood, title, description, pr
             </article>
 
             <article className="property-info">
-                <h3>{title}</h3>
+                <h3>{title}</h3>sdfsdf
                 <p>{description}</p>
             </article>
 
