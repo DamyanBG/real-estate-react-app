1f7a99842997a103df4de8753b23b3c12a6ddbfd
DamyanBG <d.dimitrov.1988@abv.bg>
2024-05-03 12:56:56 +0300

    move checkbox to separate component

--- src/pages/user/SignUp.jsx
+++ src/pages/user/SignUp.jsx
@@ -1,62 +1,72 @@
-import { useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
-import { USER_FIELDS } from '../../utils/fields';
-import { UserContext } from '../../context/UserProvider';
-import { hostUrl } from '../../utils/urls';
-import { validateField } from '../../utils/validation';
-import InputFormRow from '../../components/form/InputFormRow';
-import FormSubmitButton from '../../components/form/FormSubmitButton';
-import { ROLES_ENUM } from '../../utils/enums';
-import FormComponent from '../../components/form/FormComponent';
+import { USER_FIELDS } from "../../utils/fields";
+import { UserContext } from "../../context/UserProvider";
+import { hostUrl } from "../../utils/urls";
+import { validateField } from "../../utils/validation";
+import InputFormRow from "../../components/form/InputFormRow";
+import FormSubmitButton from "../../components/form/FormSubmitButton";
+import { ROLES_ENUM } from "../../utils/enums";
+import FormComponent from "../../components/form/FormComponent";
+import FormCheckbox from "../../components/form/FormCheckbox";
 
 export default function SignUp() {
     const [userInfo, setUserInfo] = useState({
         role: ROLES_ENUM.user,
     });
     const [validationErrors, setValidationErrors] = useState(
-        USER_FIELDS.map((uf) => uf.name).reduce((acc, curr) => ((acc[curr] = ''), acc), {})
+        USER_FIELDS.map((uf) => uf.name).reduce(
+            (acc, curr) => ((acc[curr] = ""), acc),
+            {}
+        )
     );
 
     const { setUser } = useContext(UserContext);
-    
+
     const navigate = useNavigate();
 
     const postUser = async () => {
         const urlPath =
-            userInfo.role === ROLES_ENUM.user ? 'user/register-user' : 'user/register-seller';
+            userInfo.role === ROLES_ENUM.user
+                ? "user/register-user"
+                : "user/register-seller";
 
         const { confirmPassword: _, ...postBody } = userInfo;
 
         try {
             const response = await fetch(`${hostUrl}/${urlPath}`, {
-                method: 'POST',
+                method: "POST",
                 body: JSON.stringify(postBody),
                 headers: {
-                    'Content-Type': 'application/json',
+                    "Content-Type": "application/json",
                 },
             });
 
             const json = await response.json();
 
-            console.log(json);
-
             if (!response.ok) {
-                throw new Error('Request failed!');
+                throw new Error("Request failed!");
             }
-            
+
             const newUserInfo = {
                 ...json,
-                'role': userInfo.role
-            }
+                role: userInfo.role,
+            };
 
-            localStorage.setItem('user', JSON.stringify(newUserInfo));
+            localStorage.setItem("user", JSON.stringify(newUserInfo));
             setUser(newUserInfo);
-            navigate('/');
-            toast.success('Successful Sign up!', { autoClose: 3000, pauseOnHover: false });
+            navigate("/");
+            toast.success("Successful Sign up!", {
+                autoClose: 3000,
+                pauseOnHover: false,
+            });
         } catch (err) {
-            toast.error(`Something went wrong! ${err}`, { autoClose: 3000, pauseOnHover: false });
+            toast.error(`Something went wrong! ${err}`, {
+                autoClose: 3000,
+                pauseOnHover: false,
+            });
         }
     };
 
@@ -65,13 +75,16 @@ export default function SignUp() {
 
         if (userInfo.password === userInfo.confirmPassword) {
             if (Object.keys(validationErrors).length > 0) {
-                toast.error('Please enter valid values!', { autoClose: 3000, pauseOnHover: false });
+                toast.error("Please enter valid values!", {
+                    autoClose: 3000,
+                    pauseOnHover: false,
+                });
                 return;
             }
 
             postUser();
         } else {
-            toast.error('Password don`t match. Try again!', {
+            toast.error("Password don`t match. Try again!", {
                 autoClose: 3000,
                 pauseOnHover: false,
             });
@@ -123,34 +136,27 @@ export default function SignUp() {
                 <FormComponent
                     handleOnSubmit={handleOnSubmit}
                     dataTestId="auth-form"
-                    formFields={(
-                        USER_FIELDS.map((uf) => (
-                            <InputFormRow
-                                key={uf.labelName}
-                                labelName={uf.labelName}
-                                name={uf.name}
-                                value={userInfo[uf.name]}
-                                type={uf.type}
-                                handleOnChange={handleOnChange}
-                                validationError={validationErrors[uf.name]}
-                                dataTestId={uf.name}
-                            />
-                        ))
-                    )}
-                    submitButton={(
-                        <FormSubmitButton text="Register" />
-                    )}
+                    formFields={USER_FIELDS.map((uf) => (
+                        <InputFormRow
+                            key={uf.labelName}
+                            labelName={uf.labelName}
+                            name={uf.name}
+                            value={userInfo[uf.name]}
+                            type={uf.type}
+                            handleOnChange={handleOnChange}
+                            validationError={validationErrors[uf.name]}
+                            dataTestId={uf.name}
+                        />
+                    ))}
+                    submitButton={<FormSubmitButton text="Register" />}
                     formHeaderText="Registration Form"
-                    checkboxComponent={(
-                        <article className="alert">
-                            <input
-                                data-testid="as-seller"
-                                type="checkbox"
-                                onChange={handleCheckboxChange}
-                            />
-                            <p>Sign Up as seller</p>
-                        </article>
-                    )}
+                    checkboxComponent={
+                        <FormCheckbox
+                            onCheckboxChange={handleCheckboxChange}
+                            dataTestId="as-seller"
+                            text="Sign Up as seller"
+                        />
+                    }
                 />
             </section>
         </section>
